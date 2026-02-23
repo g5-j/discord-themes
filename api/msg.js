efbb1747041113a0c001f9a9021aa48bc4916a9c
ROOD <abdovop0@gmail.com>
2026-02-24 01:11:28 +0200

    Add files via upload

--- api/msg.js
+++ api/msg.js
@@ -1,11 +1,11 @@
 import { announcementMessage, announcementTime } from "./set_msg";
 
 export default function handler(req, res) {
-    if (req.method === "GET") {
-        if (announcementMessage && (Date.now() - announcementTime <= 7000)) {
-            return res.status(200).json({ message: announcementMessage });
-        }
-        return res.status(200).json({ message: null });
+  if (req.method === "GET") {
+    if (announcementMessage && (Date.now() - announcementTime <= 7000)) {
+      return res.status(200).json({ message: announcementMessage });
     }
-    res.status(405).json({ error: "Method not allowed" });
+    return res.status(200).json({ message: null });
+  }
+  res.status(405).json({ error: "Method not allowed" });
 }
--- api/set_msg.js
+++ api/set_msg.js
@@ -1,17 +1,15 @@
 let announcementMessage = null;
 let announcementTime = 0;
 
-export default function handler(req, res) {
-    if (req.method === "POST") {
-        const data = req.body;
-        if (!data || !data.message) return res.status(400).json({ error: "No message provided" });
-
-        announcementMessage = data.message;
-        announcementTime = Date.now();
-
-        return res.status(200).json({ status: "announcement saved" });
-    }
-    res.status(405).json({ error: "Method not allowed" });
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const body = await req.json();
+    if (!body.message) return res.status(400).json({ error: "No message provided" });
+    announcementMessage = body.message;
+    announcementTime = Date.now();
+    return res.status(200).json({ status: "announcement saved" });
+  }
+  res.status(405).json({ error: "Method not allowed" });
 }
 
 export { announcementMessage, announcementTime };
